36f83b678f35091abdad2d09f3ae1f9a59694a9f
Matthew Kohn <kohney319m@gmail.com>
2020-12-23 19:20:42 -0700

    Add update selected count & calculate total price

--- script.js
+++ script.js
@@ -6,10 +6,25 @@ const movieSelect = document.getElementById('movie');
 
 const ticketPrice = +movieSelect.value;
 
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  // console.log(selectedSeatsCount);
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
 container.addEventListener('click', (e) => {
   if (
     e.target.classList.contains('seat') && 
     !e.target.classList.contains('occupied')) {
+
+    // Turns seat blue when selected
     e.target.classList.toggle('selected');
+
+    updateSelectedCount();
   }
 });
