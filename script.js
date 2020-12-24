4e481455d44d14287e55c80f9a4d97c4a99f9322
Matthew Kohn <kohney319m@gmail.com>
2020-12-23 19:15:59 -0700

    Add event listener for the container that toggles seats

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const movieSelect = document.getElementById('movie');
 
 const ticketPrice = +movieSelect.value;
 
+container.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('seat') && 
+    !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+  }
+});
